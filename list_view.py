1289b5b592e028b6dbc6f58961801d243b213901
sagar-joshi <sagar.00joshi@gmail.com>
2023-03-16 17:45:26 +0530

    refactor listview

--- list_view.py
+++ list_view.py
@@ -7,23 +7,21 @@ class ListView(QTreeView):
     def __init__(self, path):
         super().__init__()
 
+        self.__model = QStandardItemModel()
+        self.__model.setColumnCount(2)
+        self.__model.setHorizontalHeaderLabels(["Name", "Type"])
+        
+        self.updateModel(path, '')
+
+        self.setModel(self.__model)
+
+    def updateModel(self, path, itemsToShow):
         df = pd.read_excel(path)
         items = []
-
-        self.__itemsToShow = ''
         
         for index,row in df.iterrows():
-            if(self.__itemsToShow in df.columns and row[self.__itemsToShow]=='y'):
+            if(itemsToShow in df.columns and row[itemsToShow]=='y'):
                 items.append([QStandardItem(row['Name']), QStandardItem(row['Extension'])])
 
-        model = QStandardItemModel()
-        model.setColumnCount(2)
-        model.setHorizontalHeaderLabels(["Name", "Type"])
-        
         for item in items:
-            model.appendRow(item)
-
-        self.setModel(model)
-
-    def setItemsToShow(self, name):
-        self.__itemsToShow = name
+            self.__model.appendRow(item)
